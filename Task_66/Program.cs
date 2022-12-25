600ecf1e738030c1e01396ffd576bb322b3a2451
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-12-26 02:26:41 +0300

    Сделал домашнюю работу 9 семинара

---
+++ Task_66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке
+// от M до N.
+//
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRec(int n, int m)
+{
+    if (m < n) return 0;
+    else return m + SumRec(n, m - 1);
+}
+
+Console.WriteLine(SumRec(4, 8));
